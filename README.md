f23201ecc79d795c0f221d26ee054e4fad908323
sotakeiteasy <murasov96@gmail.com>
2025-05-18 13:36:44 +0300

    update app with express

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Basic site just to practice with simple node.js functions: fs, url, path etc.
+
+UPD. Add express to app
--- index.js
+++ index.js
@@ -1,33 +1,75 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+// const http = require('http');
+// const url = require('url');
+// const fs = require('fs');
 
+// const path = require("path");
+
+// const staticFiles = {
+//     '/': './pages/index.html',
+//     '/index.html': './pages/index.html',
+//     '/about.html': './pages/about.html',
+//     '/contact-me.html': './pages/contact-me.html',
+//     '/styles.css': './styles.css'
+// };
+
+// http.createServer(function (req, res) {
+//     let contentType = 'text/html';
+//     const q = url.parse(req.url, true);
+//     let __filename = staticFiles[q.pathname] || './pages/404.html';
+
+//     const extname = path.extname(__filename);
+//     if (extname === '.css') contentType = 'text/css';
+
+//     fs.readFile(__filename, function (err, data) {
+//         if (err) {
+//             res.writeHead(500, { 'Content-Type': 'text/plain' })
+//             return res.end("Server error");
+//         }
+
+//         res.writeHead(__filename === './pages/404.html' ? 404 : 200, { 'Content-type': contentType })
+//         res.write(data);
+//         return res.end()
+//     })
+// }).listen(8080, () => console.log("Server is running on - http://localhost:8080"));
+
+
+const express = require("express");
 const path = require("path");
 
-const staticFiles = {
-    '/': './pages/index.html',
-    '/index.html': './pages/index.html',
-    '/about.html': './pages/about.html',
-    '/contact-me.html': './pages/contact-me.html',
-    '/styles.css': './styles.css'
-};
+const app = express();
 
-http.createServer(function (req, res) {
-    let contentType = 'text/html';
-    const q = url.parse(req.url, true);
-    let __filename = staticFiles[q.pathname] || './pages/404.html';
+// app.get(["/", "/index.html"], (req, res) => {
+//     res.sendFile(path.join(__dirname, "pages", "index.html"))
+// })
 
-    const extname = path.extname(__filename);
-    if (extname === '.css') contentType = 'text/css';    
+// app.get("/about.html", (req, res) => {
+//     res.sendFile(path.join(__dirname, "pages", "about.html"))
+// })
 
-    fs.readFile(__filename, function (err, data) {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' })
-            return res.end("Server error");
-        }
+// app.get("/contact-me.html", (req, res) => {
+//     res.sendFile(path.join(__dirname, "pages", "contact-me.html"))
+// })
 
-        res.writeHead(__filename === './pages/404.html' ? 404 : 200, { 'Content-type': contentType })
-        res.write(data);
-        return res.end()
-    })
-}).listen(8080, () => console.log("Server is running on - http://localhost:8080"));
+// app.get("/styles.css", (req, res) => {
+//     res.sendFile(path.join(__dirname, "styles.css"))
+// })
+
+// app.use((req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, "pages", "404.html"))
+// })
+
+app.use(express.static(path.join(__dirname, "pages")));
+app.use(express.static(__dirname));
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "pages", "404.html"))
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "index.html"))
+})
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log("Server is running on - http://localhost:8080")
+})
